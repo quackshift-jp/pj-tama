ace849a5143eb3fcb2898cd523c6a3d0ed3a1168
apple <apple@appurunoMacBook-Pro.local>
2023-12-27 15:41:37 +0900

    change:データセットに休日フラグをつけられるようにする

--- src/backend/module/read_dataset.py
+++ src/backend/module/read_dataset.py
@@ -5,12 +5,30 @@ import jpholiday
 import holidays
 
 
+def attach_holiday_to_df(df: pd.DataFrame, date_column_name: str) -> pd.DataFrame:
+    jp_holidays = holidays.Japan(years=[2018, 2019])
+    df["weekend"] = df[date_column_name].apply(
+        lambda x: 1 if x.weekday() >= 5 or x in jp_holidays else 0
+    )
+    return df
+
+
+def calc_pivot(df: pd.DataFrame, index: str, columns: str) -> pd.DataFrame:
+    pivot_table = pd.pivot_table(
+        df, index=index, columns=columns, aggfunc="count", fill_value=0
+    ).reset_index()
+    # TODO:カラム名を動的に決定するように修正する
+    pivot_table.columns = ["day", "Yシャツ", "コート", "ズボン"]
+    return pivot_table
+
+
 def read_dataset(file, date_column_name: str) -> pd.DataFrame:
-    df = pd.read_csv(file, parse_dates=[date_column_name])
+    df = pd.read_csv(file, parse_dates=[date_column_name], encoding="cp932")
     df.columns = [
         col.lower() if col not in [date_column_name] else col for col in df.columns
     ]
-    return df
+    pivot_df = calc_pivot(df, index="day", columns="カテゴリ")
+    return attach_holiday_to_df(pivot_df, "day")
 
 
 def get_holidays_jp(
